2a943a595d267ef64bbd12040795c9f8306cb706
Idybaz <101640026+Idybaz@users.noreply.github.com>
2025-12-13 19:04:43 +0000

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 from paddleocr import PaddleOCR
 
 # Make sure this matches your local Poppler installation path
-POPPLER_PATH = r"C:\Program Files\Release-24.08.0-0\poppler-24.08.0\Library\bin"
+POPPLER_PATH = None
 
 # Initialize PaddleOCR once
 ocr = PaddleOCR(use_angle_cls=True, lang="en", use_gpu=False)  # Set use_gpu=True if available
